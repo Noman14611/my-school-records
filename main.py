a5f3a271e67446c21963cab2fd4a792381fe648c
Noman14611 <noumantareen316@gmail.com>
2025-06-15 14:04:53 +0500

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 # main.py
+
 import streamlit as st
 from attendanceapp import run_attendance_app
-# from feeapp import run_fee_app
-# from salaryapp import run_salary_app
 
 st.sidebar.title("📚 School Management System")
 
 app = st.sidebar.selectbox("Select App", [
-    "Attendance",
-    # "Fee Submission",
-    # "Teacher Salary"
+    "Attendance"
 ])
 
 if app == "Attendance":
     run_attendance_app()
-# elif app == "Fee Submission":
-#     run_fee_app()
-# elif app == "Teacher Salary":
-#     run_salary_app()
