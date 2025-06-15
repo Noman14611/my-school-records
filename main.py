aca7a2fa60c4bcfb3cafbf1235c2803559ac3250
Noman14611 <noumantareen316@gmail.com>
2025-06-15 14:26:10 +0500

    main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 import streamlit as st
 from attendanceapp import run_attendance_app
+from salaryapp import run_salary_app
 
-st.sidebar.title("📚 School Management System")
+st.set_page_config(page_title="School Management System", layout="centered")
 
-app = st.sidebar.selectbox("Select App", [
-    "Attendance"
-])
+st.sidebar.title("🏫 School Management System")
+app = st.sidebar.selectbox("📂 Select Module", ["Attendance", "Teacher Salary"])
 
 if app == "Attendance":
     run_attendance_app()
+elif app == "Teacher Salary":
+    run_salary_app()
