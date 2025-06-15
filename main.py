51206cd735310ec640a481565871a88d6401810d
Noman14611 <noumantareen316@gmail.com>
2025-06-15 13:21:10 +0500

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 # main.py
 
 import streamlit as st
-from attendance import run_attendance_app
-# from feeapp import run_fee_app      # ✅ Future ready
-# from salaryapp import run_salary_app  # ✅ Future ready
+from attendanceapp import run_attendance_app
+# from feeapp import run_fee_app
+# from salaryapp import run_salary_app
 
-# Title in sidebar
 st.sidebar.title("📚 School Management System")
 
-# Dropdown to select app
 app = st.sidebar.selectbox("Select App", [
     "Attendance",
     # "Fee Submission",
     # "Teacher Salary"
 ])
 
-# Load selected app
 if app == "Attendance":
     run_attendance_app()
 # elif app == "Fee Submission":
